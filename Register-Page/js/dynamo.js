bd2c168a9195781b6b7bbb9c3b7c26ba050886b2
JRGearheart21 <87998334+JRGearheart21@users.noreply.github.com>
2021-12-27 12:43:01 -0500

    1.1.3
    1.1.3 added back in insertNewuser

--- Register-Page/js/dynamo.js
+++ Register-Page/js/dynamo.js
@@ -2,7 +2,36 @@ src="js/config.js"
 src="js/amazon-cognito-identity.min.js"
 src="https://cdnjs.cloudflare.com/ajax/libs/web-socket-js/1.0.0/web_socket.min.js"
 
+function insertNewUser(user_id) {
+    AWS.config.update({
+        region: "us-east-2",
+        accessKeyId: "AKIASM2S677I6DGOD7OA",
+        secretAccessKey: "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"
+    });
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    var data = { 
+		UserPoolId : _config.cognito.userPoolId,
+        ClientId : _config.cognito.clientId
+    };
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
 
+    var params = {
+        TableName :"fasniper_users",
+        Item:{
+            "fas_user_ID": user_id.username,
+            "AWS_client_ID": user_id.username
+        }
+    };
+    docClient.put(params, function(err, data) {
+        if (err) {
+            console.log("Unable to add item");
+        } else {
+            console.log(" Put User succeeded!!!!");
+            window.open('login.html','_self');
+        }
+    });
+
+} 
 
 function insertUser(user_id, auth_code) {
     
@@ -49,6 +78,7 @@ function AWSupload(user_id,auth_code){
     }); 
     
 }
+
 function insertItem(waiverID, rosterID, priority) {
     AWS.config.update({
         region: "us-east-2",
