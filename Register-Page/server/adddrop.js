e05369ab874e9623796501c9fde013a1c5e348c5
JRGearheart21 <87998334+JRGearheart21@users.noreply.github.com>
2022-09-18 22:02:10 -0400

    update token for adddrop
    works!

--- Register-Page/server/adddrop.js
+++ Register-Page/server/adddrop.js
@@ -4,9 +4,10 @@ var fs = require('fs');
 var options = {
   'method': 'POST',
   'hostname': 'fantasysports.yahooapis.com',
-  'path': '/fantasy/v2/league/nfl.l.53605/transactions',
+  'path': '/fantasy/v2/league/nfl.l.1461770/transactions',
   'headers': {
-    'Authorization': 'Bearer 1XIh8p6b5hxw51qq2JSunE_3jjSItto0yDD.ngRdi7wcsbFcl_Bmf6tz_suNXmV5PDOb4bcwi4g6o8a3f5GQLJjLgDrk67At7byGp9ZzdO.EQFSGtuha2skGxscnLNjHiqVkL3tAnE_SFU.B85XY5sKPdYnSRAHfanZxAD0fNXDfgJtvXKTWXg86OpW1At1rbfMg1pXIMb.gln34gRE4GSEarH9ACtP8aehB0k89GvxZPsD6HXdW9mq0ZE9CCpeb5w9U1GqE6OMeUwqHeby74KkzsHnC0jHuSnGXHCJCX.lw9gTAIgW.iBvmvosb2qmKVl6J85GahKnV_hEMLYphX59C1ht3qG8NZXYpKfX6z7pJO4rh__6mUAaYi4uF6jX.HIdoDpcr6pGBEc1vJy8_hI.F.BCjtHMr3gRtUCZAbyBceC27njGVsnRlSHqbbqvjtCLrybElf4doLCRwekR39am.om6yinJhx6HAoAXVV3H7N5Tu3tu14JA2OWl9_0SiiCuOGUq84EdZRzMgqeAesJv05vYCcUGIqSvsGdwBx3q1DMIuPzxsjUwJedTqiPNNuvHJ7gyAejmZNCYbAEba4CkVd346PC_0bpnioTPqayqst76MZoza.K.AfcV015ukhvXhr.LkksnsGNElJkmTyZOLvT2DVx2U3elZNt1.7qkUC1a5f6uQCo1VjfeTMr1nGbU2RCDwQBBPO2a8flrvc.u_BBNkc95MBff9ETGvd3QoGk1RRT43k6dOyxQjOpciRyDQHFGZozSTkXJgJ5IPVc4X8.rVoXYtbE.Wz4UEsFTbKNaXnMJr1LksF9hdCRHc.0NEPBasviovrPY2ZBdZedm.2ym11dLjLuBMOBjmnBvV1EbLPk56.ePNFN_JO6OX.muEiVzO4q0j9AH.M2tuJVnbhMRKGzNodj9DUMj8X_.CYWqobFuZ6dg0VdnchkVK3YqqdCxYF69yVoVsbNB2o6FJKgRxPaVgTm1ZH6gS4DYMOSHPT1YEdYzyuEol2GVD2Te5FtZKEvL.RqoWjCRO3_qlWg0amLzw9plqe8jnKxqQijNBAnw9wD1k30_lBR7nhiKaqyi2i9j.KMpoTrfX',
+    'Authorization': 'Bearer vEGaKqKcpV14gWrrfDoEvvFqxhmebAntp4diUjvVIpygeAZqWMEb6qZhGfbB5o2coA1Kk5hH1UezwuwVMpFdxPScn1enLMnw3dJ.I9oIQi__jkeCDAxYwIJfkByZ2SwXEZAbjI64hDyJUf4HMatPKlpas1ruEZgVTwgYx826xvDETOX0MW5RYCvnEYaQWAjcQVmdIRCDu4UE.eB8H912FpNEdagH72lVsfFjt5lh_5jAghUQ0PowQPcUWOpwpmN48AGBCyiUHmiQ4HDnBqSW_Hz90lkqmybfEavU_lHzGGL6h4sS0fBnOvmNuF1g5ksfy_iq8i5Amks7gDE0fad7ce1.6I3jAqftrEj.k0jRvWDNJTawdJ_9bqysmuFf1kOm.Af6ZxYO7l3Qt0WG0GIHtxQCSPnP6k.UyBGUP_vT.acAWwk9P2RD79Qt87I9wRxRsuSISVbWL7RdrFqtQbS.zBODhaU4idzF8hmeTNw8uBexP.nS_d9vzEGhk36NAdWHs3dyOEuj0K7ZhNCjfBtBerQyC6zinUeYjESjVdnnTWcPCEgU0sm66Y520Q7PJPCferTqy..xtookMcA3QztGylHGwdTCLJSOqA9hupqkcM7SjHlJgcbC8VZvV3BVW_eOYFivM8QCuzZCqsPpmZuLHgN2GjWHPgwhvVbtQZQgcUtb98wj.VMK6KLONL8ciX5viXqihpWifxggnRPD8iGscIunRQlGV1KE5FJpxCAcBf7B44qoaYd6W_EMSwI4xiRRe4vVzahu0JZBOq7nq_ByTS9OHvMhFM4LVEXYTvHZBdhM_72KNlDHO0gtpfWUNm1UKxYN7blfD1nPbpaMsq7Tj4QZxS8RbzHJdJ9zpivWqqqg74qYNeFD5GlaP9G9TXM22Zuz4H9OsqbvoHS7YrHLUz0L47bFyxR1wC0CwFE3NlpMx922lv7PwT0qR9CqjmaWuHzLcCRnQARCaUp9YekFfClyO4uq5gI9xoHk3XYm6OQhtLqir0lBDYy2I05CXMLzkNphEBZngM5Y03Vu5S2J3ZhraaXdorb0o6pUNVk-',
+              //^^access token
     'Content-Type': 'application/xml'
   },
   'maxRedirects': 20
@@ -29,7 +30,7 @@ var req = https.request(options, function (res) {
   });
 });
 
-var postData = "<fantasy_content>\r\n<transaction>\r\n<type>add/drop</type>\r\n<players>\r\n<player>\r\n<player_key>nfl.p.29236</player_key>\r\n<transaction_data>\r\n<type>add</type>\r\n<destination_team_key>nfl.l.53605.t.9</destination_team_key>\r\n</transaction_data>\r\n</player>\r\n<player>\r\n<player_key>nfl.p.33391</player_key>\r\n<transaction_data>\r\n<type>drop</type>\r\n<source_team_key>nfl.l.53605.t.9</source_team_key>\r\n</transaction_data>\r\n</player>\r\n</players>\r\n</transaction>\r\n</fantasy_content>";
+var postData = "<fantasy_content>\r\n<transaction>\r\n<type>add/drop</type>\r\n<players>\r\n<player>\r\n<player_key>nfl.p.32723</player_key>\r\n<transaction_data>\r\n<type>add</type>\r\n<destination_team_key>nfl.l.1461770.t.1</destination_team_key>\r\n</transaction_data>\r\n</player>\r\n<player>\r\n<player_key>nfl.p.30132</player_key>\r\n<transaction_data>\r\n<type>drop</type>\r\n<source_team_key>nfl.l.1461770.t.1</source_team_key>\r\n</transaction_data>\r\n</player>\r\n</players>\r\n</transaction>\r\n</fantasy_content>";
 
 req.write(postData);
 
